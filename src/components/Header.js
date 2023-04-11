8725e074d8d5803a77ba684bd66286bdada52212
santy <mynameissantiagogarcia@gmail.com>
2023-04-11 09:47:12 -0400

    added hamburger menu functionality to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../images/AEyeLogo.png";
 import "../styles/Header.css";
 import { observer } from "mobx-react-lite";
 import { store, logout, initialize } from "../store";
 
 const Header = observer(() => {
+    const [isHamburgerClicked, setIsHamburgerClicked] = useState(false);
+
+    useEffect(() => {
+        function handleResize() {
+            const horMenu = document.getElementsByClassName("Header-right")[0];
+            const vertMenu = document.getElementsByClassName(
+                "Header-right-vertical"
+            )[0];
+            const hamMenu =
+                document.getElementsByClassName("Header-hamburger")[0];
+            if (window.innerWidth < 400 && isHamburgerClicked) {
+                showElement(vertMenu);
+                showElement(hamMenu);
+                hideElement(horMenu);
+            } else if (window.innerWidth < 400 && !isHamburgerClicked) {
+                showElement(hamMenu);
+                hideElement(horMenu);
+                hideElement(vertMenu);
+            } else if (window.innerWidth >= 400) {
+                setIsHamburgerClicked(false);
+                showElement(horMenu);
+                hideElement(hamMenu);
+                hideElement(vertMenu);
+            }
+        }
+
+        window.addEventListener("resize", handleResize);
+
+        // Clean up event listener when component is unmounted
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    const hideElement = (element) => {
+        element.style.display = "none";
+    };
+
+    const showElement = (element) => {
+        element.style.display = "flex";
+    };
+
     const renderAuthButton = () => {
         initialize();
         if (store.isLoggedIn) {
@@ -33,13 +75,35 @@ const Header = observer(() => {
         logout();
     };
 
+    const handleMenuClick = () => {
+        const vertMenu = document.getElementsByClassName(
+            "Header-right-vertical"
+        )[0];
+        const hamMenu = document.getElementsByClassName("Header-hamburger")[0];
+        if (isHamburgerClicked == false) {
+            setIsHamburgerClicked(true);
+            showElement(vertMenu);
+        } else {
+            setIsHamburgerClicked(false);
+            hideElement(vertMenu);
+        }
+    };
+
     return (
         <div className="Header">
             <div className="Header-left">
                 <img src={logo} className="Header-logo" alt="logo" />
                 <span className="Header-app-name">Ai Resume</span>
             </div>
+            <div className="Header-hamburger" onClick={handleMenuClick}>
+                &#9776;
+            </div>
             <div className="Header-right">
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                {renderAuthButton()}
+            </div>
+            <div className="Header-right-vertical">
                 <a href="/">Home</a>
                 <a href="/about">About</a>
                 {renderAuthButton()}
