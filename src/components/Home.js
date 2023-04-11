93273426bdecc9709179e1fe235b2260dc50a7b7
santy <mynameissantiagogarcia@gmail.com>
2023-04-11 02:39:59 -0400

    added chatbox image to homepage and changed the background design

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,43 +8,50 @@ function Home() {
 
     return (
         <div className="home-container">
-            <div className="content-container">
-                <h4>Artificial Intelligence Resume Builder</h4>
-                <h1>
-                    Create your perfect resume in minutes with the help of AI.
-                </h1>
-                <p>
-                    Say goodbye to endless hours spent perfecting your resume -
-                    with Ai Resume, our AI-powered platform makes creating a
-                    professional resume fast and easy - Try it now!
-                </p>
-                <button onClick={CreateResume}>Create Resume</button>
-                <div class="chatbox">
-                    <div class="message sent">
-                        <h3>
-                            Write a bio section for someone who wants to be a
-                            software engineer.
-                        </h3>
-                    </div>
-                    <div class="message received">
-                        <h3>
-                            "Highly motivated and detail-oriented software
-                            engineer with [number of years] years of experience
-                            in [programming languages or tools]. Adept at
-                            developing and implementing complex software
-                            solutions to meet business requirements, with a
-                            proven track record ..."
-                        </h3>
-                    </div>
-                    <div class="message sent">
-                        <h3>Can you mention my experience with JavaScript?</h3>
-                    </div>
-                    <div class="message received">
-                        <h3>...</h3>
+            <div className="background-image">
+                <div className="content-container">
+                    <h4>Artificial Intelligence Resume Builder</h4>
+                    <h1>
+                        Create your perfect resume in minutes with the help of
+                        AI.
+                    </h1>
+                    <p>
+                        Say goodbye to endless hours spent perfecting your
+                        resume - with Ai Resume, our AI-powered platform makes
+                        creating a professional resume fast and easy - Try it
+                        now!
+                    </p>
+                    <button onClick={CreateResume}>Create Resume</button>
+                    <h4>
+                        Chat with the AI assistant for expert resume
+                        recommendations
+                    </h4>
+                    <div className="chatbox">
+                        <div className="message sent">
+                            <h3>
+                                Write a short and professional bio for a junior
+                                software engineer.
+                            </h3>
+                        </div>
+                        <div className="message received">
+                            <h3>
+                                As a junior software engineer with a strong
+                                passion for technology and problem-solving, I am
+                                eager to learn and grow in the field. With
+                                experience in ...{" "}
+                            </h3>
+                        </div>
+                        <div className="message sent">
+                            <h3>
+                                Can you mention my experience with JavaScript?
+                            </h3>
+                        </div>
+                        <div className="message received">
+                            <h3>...</h3>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="background-image"></div>
         </div>
     );
 }
