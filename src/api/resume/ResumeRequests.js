d92638a9312fc9a0fbc00bf86cab2100d0af0f03
santy81855 <mynameissantiagogarcia@gmail.com>
2023-06-28 18:03:00 -0400

    made the api-url more configurable with an .env file

--- src/api/resume/ResumeRequests.js
+++ src/api/resume/ResumeRequests.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const URL =
-    process.env.NODE_ENV === "production"
-        ? "http://ec2-3-135-220-37.us-east-2.compute.amazonaws.com:3000"
-        : "http://localhost:3000";
+const URL = process.env.REACT_APP_API_URL;
 
 const createResume = (data) => {
     console.log(data);
